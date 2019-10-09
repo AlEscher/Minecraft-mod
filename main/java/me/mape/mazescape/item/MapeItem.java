cd59d153f5fdf98310e03b4bb0c14393e582fa00
Alessandro <alessandroescher@gmx.net>
2019-10-09 17:54:24 +0200

    Item classes
    Added generic Wrapper class for items, aswell as the Key

---
+++ main/java/me/mape/mazescape/item/MapeItem.java
@@ -0,0 +1,14 @@
+package me.mape.mazescape.item;
+
+import net.minecraft.item.Item;
+
+/*
+ * Wrapper class for this mod's items
+ */
+public class MapeItem extends Item{
+	
+	public MapeItem() {
+		super();
+	}
+
+}
---
+++ main/java/me/mape/mazescape/item/MazeScapeKey.java
@@ -0,0 +1,14 @@
+package me.mape.mazescape.item;
+
+import net.minecraft.item.Item;
+
+/*
+ * The key that is needed in game to complete a stage
+ */
+public class MazeScapeKey extends MapeItem{
+	
+	public MazeScapeKey() {
+		super();
+	}
+
+}
