c74135666b59e698a6d47ad831549cf173260979
ge25vag <maximilian.pursche@tum.de>
2019-10-10 00:01:36 +0200

    Updated Labyrinth for better performance

--- main/java/me/mape/mazescape/utility/MazeTest.java
+++ main/java/me/mape/mazescape/utility/MazeTest.java
@@ -29,7 +29,6 @@ public class MazeTest {
 		}
 	}
 
-	
 	public MazeTest(int x, int y, int length, int minLength) {
 		this.max_y = y;
 		this.max_x = x;
@@ -37,15 +36,14 @@ public class MazeTest {
 		this.matrix = new int[y][x];
 		generateMainPath(length, minLength);
 	}
-	
+
 	public int[][] getMatrix() {
 		return matrix;
 	}
 
 	private boolean validateCoor(int x, int y) {
-		if (x < max_x-1 && y < max_y-1 && x > 0 && y > 0 && matrix[y][x] == 0) {
+		if (x < max_x - 1 && y < max_y - 1 && x > 0 && y > 0 && matrix[y][x] == 0) {
 			int nummainpath = matrix[y + 1][x] + matrix[y - 1][x] + matrix[y][x + 1] + matrix[y][x - 1];
-			//System.out.println(nummainpath);
 			if (nummainpath > 0)
 				return false;
 			else
@@ -58,66 +56,86 @@ public class MazeTest {
 		for (int i = 0; i < matrix.length; i++) {
 			System.out.print("[");
 			for (int k = 0; k < matrix[i].length; k++) {
-//				if(matrix[i][k] == 1) {
-//					System.err.print(" " + matrix[i][k] + ",");
-//				}
-//				if(matrix[i][k] == 0) {
-					System.out.print(" " + matrix[i][k] + ",");
-				//}
+
+				System.out.print(" " + matrix[i][k] + ",");
+
 			}
 			System.out.println("]");
 		}
 	}
 
 	private void generateMainPath(int lenght, int minLength) {
-		int steps=0;
+		int steps = 0;
 		int start = (int) (Math.random() * (max_x - 2));
 		int current_x = 1 + start;
 		int current_y = 1;
 		Coordinate current = new Coordinate(current_x, current_y);
-		startcoor=current;
+		startcoor = current;
 		current.print();
 		Coordinate last = null;
 		for (int i = 0; i < lenght; i++) {
 			if (Math.random() < 0.4) {
 				if (Math.random() < 0.5 && validateCoor(current_x + 1, current_y)) {
-					System.out.println("x + 1");
+					//System.out.println("x + 1");
 					current_x++;
 
 				} else if (validateCoor(current_x - 1, current_y)) {
-					System.out.println("x - 1");
+					//System.out.println("x - 1");
 					current_x--;
 				}
 			} else {
 				if (Math.random() < 0.6 && validateCoor(current_x, current_y + 1)) {
-					System.out.println("y + 1");
+					//System.out.println("y + 1");
 					current_y++;
 				} else if (validateCoor(current_x, current_y - 1)) {
-					System.out.println("y - 1");
+					//System.out.println("y - 1");
 					current_y--;
 				}
 			}
 			if (current_x != current.getX() || current_y != current.getY()) {
 				last = current;
-				matrix[current.getY()][current.getX()] = 1;
+				if (steps == 0)
+					matrix[current.getY()][current.getX()] = 4;
+				else
+					matrix[current.getY()][current.getX()] = 1;
 				steps++;
 				current = new Coordinate(current_x, current_y);
 
 			}
-			//current.print();
+			// current.print();
 		}
-		matrix[current.getY()][current.getX()]=3;
-		if(steps<minLength) {
-			matrix=new int [max_y][max_x];
+		matrix[current.getY()][current.getX()] = 3;
+		if (steps < minLength) {
+			matrix = new int[max_y][max_x];
 			generateMainPath(lenght, minLength);
 		}
 	}
 
-	public void generateSidePath() {
-		int begin=(int) (Math.random()*6)+1;
-		
+	private Coordinate nextMain(Coordinate c) {
+		int x = c.getX();
+		int y = c.getY();
+		Coordinate result;
+		if (matrix[y + 1][x] == 1||matrix[y + 1][x] == 3 ) {
+
+		}
+		if (matrix[y - 1][x] == 1||matrix[y + 1][x] == 3) {
+
+		}
+		if (matrix[y][x+1] == 1||matrix[y][x+1] == 3) {
+
+		}
+		if (matrix[y][x-1] == 1||matrix[y][x-1] == 3) {
+
+		}
+		result = new Coordinate(x, y);
+		return result;
 	}
-	
+
+	public void generateSidePath() {
+		int begin = (int) (Math.random() * 6) + 1;
+
+	}
+
 //	public static void main(String[] args) {
 //		MazeTest m1 = new MazeTest(30, 30);
 //		// m1.printmatrix();
