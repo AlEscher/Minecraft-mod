672cd26e895e40df72e7d385713d2c29c0050310
Alessandro <alessandroescher@gmx.net>
2019-10-09 15:10:56 +0200

    Updated Modclass information
    Updated modclass info, added mod instance

--- main/java/me/mape/mazescape/MazeScape.java
+++ main/java/me/mape/mazescape/MazeScape.java
@@ -3,29 +3,29 @@ package me.mape.mazescape;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MazeScape.MODID, name = MazeScape.NAME, version = MazeScape.VERSION)
+public class MazeScape
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "mazescape";
+    public static final String NAME = "MazeScape";
+    public static final String VERSION = "1.12.2-1.0";
 
-    private static Logger logger;
-
+    @Instance("mazescape")
+    public static MazeScape instance;
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
